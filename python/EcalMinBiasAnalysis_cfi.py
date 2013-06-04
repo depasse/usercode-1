ae6cd207bbb0f328bae05101b4f5c5ab020cb12c
Fabio Cossutti <fabiocos@lxbuild170.cern.ch>
2013-06-04 22:35:27 +0200

    Extend PU reweighting to ECAL/HCAL hits analyses

--- python/EcalMinBiasAnalysis_cfi.py
+++ python/EcalMinBiasAnalysis_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-ecalMinBiasAnalysis = cms.EDAnalyzer("EcalMinBiasAnalysis",
+stdEcalAnaPset = cms.PSet(
     recoVertexCollection = cms.InputTag("offlinePrimaryVertices"),
     pixelClusterCollection = cms.InputTag("siPixelClusters"),
     ebRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEB"),                                     
@@ -8,3 +8,7 @@ ecalMinBiasAnalysis = cms.EDAnalyzer("EcalMinBiasAnalysis",
     eeDigiCollection = cms.InputTag("ecalDigis","eeDigis"),
     vtxSel = cms.int32(20)                                     
 )                                
+
+ecalMinBiasAnalysis = cms.EDAnalyzer("EcalMinBiasAnalysis",
+    stdEcalAnaPset
+)                                     
